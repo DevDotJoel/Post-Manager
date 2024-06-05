9806d7725bf12608fa78f098a26f1e9a88e7a439
Joel <decskill.jferreira@ca-seguros.pt>
2024-06-05 11:43:54 +0100

    aedaed

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,8 @@
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ fetch }: any) {
-	const response = await fetch('/data-api/rest/Post');
+export async function load({ fetch, url }: any) {
+	console.log(url.href + 'data-api/rest/Post');
+	const response = await fetch(url.href + 'data-api/rest/Post');
 	const persons = await response.json();
-	console.log(persons);
 	return {
 		persons
 	};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+
+    /** @type {import('./$types').PageData} */
+    export let data;
+   console.log(data)
+</script>
 <h1>FRANCISCO</h1>
 <p>ESTÁ APRENDER C#!! MASSACRE <a href="https://github.com/fraciscro">CONTA GITHUB DELEEEE</a> </p>
