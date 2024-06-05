357994c016508d2abeb7235f839459a24aaab64c
Joel <decskill.jferreira@ca-seguros.pt>
2024-06-05 11:55:28 +0100

    aedaaed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-
-    /** @type {import('./$types').PageData} */
-    export let data;
-   console.log(data)
+/** @type {import('./$types').PageData} */
+   export let data;
+   const {persons}=data
+   console.log(persons)
 </script>
 <h1>FRANCISCO</h1>
 <p>ESTÁ APRENDER C#!! MASSACRE <a href="https://github.com/fraciscro">CONTA GITHUB DELEEEE</a> </p>
