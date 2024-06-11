4b3e742205ba2ceb68ac3272733d8c7e99c310d9
Joel <decskill.jferreira@ca-seguros.pt>
2024-06-11 09:36:14 +0100

    aedad

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,8 @@
 export async function load({ fetch }: any) {
 	const response = await fetch('data-api/rest/Post');
 	const data = await response.json();
+	const persons = data.value;
 	return {
-		data
+		persons
 	};
 }
